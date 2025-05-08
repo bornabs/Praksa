fa2c8a5c546f9154bf5483e1aca9fd2db0abd1f8
bornabs <bacac.borna2@gmail.com>
2025-05-08 16:12:48 +0200

    izgradnja test stranice

---
+++ moj_projekt/src/pages/TestPage.vue
@@ -0,0 +1,16 @@
+<!-- src/pages/TestPage.vue -->
+<script setup>
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+<div class="w-full min-h-screen bg-blue-500 text-white p-6">
+    <div class="max-w-screen-xl mx-auto">
+      <div class="flex flex-col items-center gap-4">
+        <h1 class="text-2xl font-bold">Testna stranica</h1>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  </div>
+
+</template>
