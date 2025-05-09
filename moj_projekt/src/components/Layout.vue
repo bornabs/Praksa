0e43a8cf4f395fde059512f96048533c2846dbc1
bornabs <bacac.borna2@gmail.com>
2025-05-09 11:29:27 +0200

    izgradnje layout.vue

---
+++ moj_projekt/src/components/Layout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from './Navbar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+    <main class="flex-1 p-6">
+      <slot />
+    </main>
+    <footer class="text-center text-sm text-gray-500 py-4">
+      © 2025 TurnirApp
+    </footer>
+  </div>
+</template>
