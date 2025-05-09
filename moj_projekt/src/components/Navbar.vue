594055547e2c1c664b8ecd9219a32101719189cd
bornabs <bacac.borna2@gmail.com>
2025-05-09 14:28:48 +0200

    izgradnja inicijalne postave aplikacije

--- moj_projekt/src/components/Navbar.vue
+++ moj_projekt/src/components/Navbar.vue
@@ -3,6 +3,8 @@
     <div class="text-xl font-semibold">🏀 Aplikacija za turnire 🏀</div>
     <ul class="flex space-x-4">
       <li><router-link to="/" class="hover:text-blue-400">Početna</router-link></li>
+      <li><router-link to="/teams" class="hover:text-blue-400">Timovi</router-link></li>
+      <li><router-link to="/tournaments" class="hover:text-blue-400">Turniri</router-link></li>
       <li><router-link to="/test" class="hover:text-blue-400">Test</router-link></li>
       <li><router-link to="/about" class="hover:text-blue-400">O nama</router-link></li>
     </ul>
